21c58feea90f00aa1979c56fda66b3d8685d0490
Alxhr0 <92590433+Alxhr0@users.noreply.github.com>
2024-07-26 01:09:04 +0200

    Add more packages

--- build.sh
+++ build.sh
@@ -15,11 +15,11 @@ RELEASE="$(rpm -E %fedora)"
 wget https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo -O /etc/yum.repos.d/solopasha-hyprland-fedora-40.repo
 
 # this installs a package from fedora repos
-rpm-ostree install hyprland waybar thunar thunar-archive-plugin thunar-volman swww kitty brightnessctl sddm xdg-desktop-portal-hyprland neovim htop rofi-wayland
+rpm-ostree install hyprland waybar thunar thunar-archive-plugin thunar-volman swww kitty brightnessctl sddm xdg-desktop-portal-hyprland neovim htop rofi-wayland NetworkManager-wifi NetworkManager linux-firmware fastfetch flatpak pipewire wireplumber pipewire-pulseaudio pavucontrol 
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl enable podman.socket sddm.service NetworkManager
